6a399ccb2754f6e786f131a56ab018d9107ab557
liuxianTODO <2536596010@qq.com>
2022-09-20 15:57:58 +0800

    修改两个类

--- guest/src/main/java/com/guest/pojo/po/Menu.java
+++ guest/src/main/java/com/guest/pojo/po/Menu.java
@@ -1,31 +1,19 @@
 package com.guest.pojo.po;
 
-public class Menu {
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class Menu extends Model<Menu> {
+    @TableId(value = "dish_id")
     String dish_id;//菜编号
     String dish_name;//菜名
     float dish_price;//菜价
 
-    public String getDish_id() {
-        return dish_id;
-    }
-
-    public void setDish_id(String dish_id) {
-        this.dish_id = dish_id;
-    }
-
-    public String getDish_name() {
-        return dish_name;
-    }
-
-    public void setDish_name(String dish_name) {
-        this.dish_name = dish_name;
-    }
-
-    public float getDish_price() {
-        return dish_price;
-    }
-
-    public void setDish_price(float dish_price) {
-        this.dish_price = dish_price;
-    }
 }
