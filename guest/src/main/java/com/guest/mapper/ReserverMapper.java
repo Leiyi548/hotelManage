4c227a32052563b3a20fc38756e00885d3edea5a
ddhandsome <964005436@qq.com>
2022-09-21 17:18:08 +0800

    增加了通过名字更新消费者的功能

--- guest/src/main/java/com/guest/mapper/ReserverMapper.java
+++ guest/src/main/java/com/guest/mapper/ReserverMapper.java
@@ -10,5 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReserverMapper  extends BaseMapper<Reserver> {
    int addReserver(Reserver reserver);
-
+   Reserver getReserverByName(String ReserverName);
 }
