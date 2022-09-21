a464b0a999a64a8aed031a15bf9dc5d1b0687b59
86150 <linglong@qq.com>
2022-09-21 12:36:37 +0800

    增加点菜和结账功能,修改部分配置

--- guest/src/main/java/com/guest/pojo/vo/ReserverMenu.java
+++ guest/src/main/java/com/guest/pojo/vo/ReserverMenu.java
@@ -1,5 +1,6 @@
 package com.guest.pojo.vo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,10 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReserverMenu {
-    @TableId(value = "ReserverMenu_Id")
-    int id;
+    @TableId(value = "id",type = IdType.AUTO)
+    Integer id;
     String reserver_name;//预订人姓名
     String reserver_tel;//预订人电话
-    int dish_id;//菜品id
+    Integer dish_id;//菜品id
     int state;//结账状态  1结账  0未结账
 }
