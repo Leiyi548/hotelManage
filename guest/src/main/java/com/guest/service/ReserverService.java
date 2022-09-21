59760b0c8f0a5eb9aa56c7d8c36360d75e5d69ec
liuxianTODO <2536596010@qq.com>
2022-09-21 09:46:26 +0800

    新增预订者信息的删除和查找方法

--- guest/src/main/java/com/guest/service/ReserverService.java
+++ guest/src/main/java/com/guest/service/ReserverService.java
@@ -4,9 +4,14 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.guest.pojo.po.Reserver;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 public interface ReserverService extends IService<Reserver> {
    int addReserver(Reserver reserver);
+   List searchReserverByName(String reserver_name);
+   int deleteReserverByTel(String reserver_tel);
+   int deleteReserverByTel2(String reserver_tel);
 
 }
